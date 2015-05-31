02368b86fb029dea6e642e7a3d1f8df78820a2bb
poilkjmn22 <1042260934@qq.com>
2015-05-31 13:02:53 +0800

    change.log

--- canvasUtil.js
+++ canvasUtil.js
@@ -1,4 +1,4 @@
-var CanvasUtil = function(canvasid){
+var self.Canvas = function(canvasid){
 	this.canvas =  document.getElementById(canvasid) || canvasid;
 	this.context =  this.canvas.getContext("2d");
 	this.fillStyle =  "#888";
@@ -7,15 +7,15 @@ var CanvasUtil = function(canvasid){
 /*	
 * get,set
 */
-CanvasUtil.prototype.init =  function(){
+self.Canvas.prototype.init =  function(){
 }
 
-CanvasUtil.prototype.resize =  function(w,h){
+self.Canvas.prototype.resize =  function(w,h){
 	if(! this.canvas){ return;}
 	this.canvas.width = w;
 	this.canvas.height = h;
 }
 
-CanvasUtil.prototype.clear =  function(){
+self.Canvas.prototype.clear =  function(){
 	this.canvas.clearRect(0, 0, this.canvas.width, this.canvas.height);
 }
